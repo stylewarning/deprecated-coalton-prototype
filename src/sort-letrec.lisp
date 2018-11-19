d6f6904360161e87b3f7c02cea8ee1c32cfe4610
Robert Smith <robert@stylewarning.com>
2018-11-18 17:03:50 -0800

    detect self-referential vars in letrec sorting

--- src/sort-letrec.lisp
+++ src/sort-letrec.lisp
@@ -23,37 +23,51 @@
         :when (null deps)
           :collect node))
 
-(defun tsort (dag)
+(defun clean-up-self-referential (dag)
+  (loop :for thing :in dag
+        :for (node . deps) := thing
+        :when (member node deps)
+          :collect (progn
+                     (setf (cdr thing) (delete node deps))
+                     node)
+            :into self-referential
+        :finally (return (values dag self-referential))))
+
+(defun tsort (input-dag)
   "Find the topological sort of GRAPH destructively. Return two values:
 
 1. The best topological sort that could be done (a list of <node>s)
 
-2. The rest of the unsortable DAG that _only_ contains cycles."
-  (let* ((sorted nil)                   ; Final sorted list.
-         (sinks  (list-sinks dag)))     ; Sinks in the graph.
-    (loop :while (not (null sinks))
-          :do (progn
-                ;; Remove the sinks.
-                (setf dag (delete-if (lambda (x) (null (cdr x))) dag))
-                
-                ;; Get the next sink.
-                (let ((sink (pop sinks)))
-                  
-                  ;; Add it to the sorted list.
-                  (push sink sorted)
-                  
-                  ;; For every node/neighborhood...
-                  (dolist (node dag)
-                    
-                    ;; Remove the sink from the dependencies if any
-                    ;; exist.
-                    (setf (cdr node) (delete sink (cdr node)))
-                    
-                    ;; If we have no more dependencies, add it to the
-                    ;; sinks.
-                    (when (null (cdr node))
-                      (push (car node) sinks)))))
-          :finally (return (values (nreverse sorted) dag)))))
+2. The rest of the unsortable DAG that _only_ contains cycles.
+
+3. The nodes that were self-referential.
+"
+  (multiple-value-bind (dag self-referential)
+      (clean-up-self-referential input-dag)
+    (let* ((sorted nil)                   ; Final sorted list.
+           (sinks  (list-sinks dag)))     ; Sinks in the graph.
+      (loop :while (not (null sinks))
+            :do (progn
+                  ;; Remove the sinks.
+                  (setf dag (delete-if (lambda (x) (null (cdr x))) dag))
+
+                  ;; Get the next sink.
+                  (let ((sink (pop sinks)))
+
+                    ;; Add it to the sorted list.
+                    (push sink sorted)
+
+                    ;; For every node/neighborhood...
+                    (dolist (node dag)
+                      ;; Remove the sink from the dependencies if any
+                      ;; exist.
+                      (setf (cdr node) (delete sink (cdr node)))
+
+                      ;; If we have no more dependencies, add it to the
+                      ;; sinks.
+                      (when (null (cdr node))
+                        (push (car node) sinks)))))
+            :finally (return (values (nreverse sorted) dag self-referential))))))
 
 (defun letrec-bindings-to-dag (vars vals)
   ;; VARs are the variables being bound
